62b9d53aa798363decd7b8795212d26b276bb351
nikitasontake <101354370+nikitasontake@users.noreply.github.com>
2025-03-26 23:14:19 +0530

    Initial commit

---
+++ java/Assignment4.java
@@ -0,0 +1,10 @@
+public class Assignment4 {
+    public static void main(String[] args){
+        Assignment4 a4 = new Assignment4();
+        a4.test();
+    }
+
+    public void test(){
+        System.out.println("IntelliJ first Project");
+    }
+}
